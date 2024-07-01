19c0a9c44e800ae4f2320f16e4413f6a5bd05b76
lacel <mundusdragon@gmail.com>
2024-07-02 00:10:11 +0300

    реализована анимация и стили лайтов заднего фона авторизации

--- src/react/components/containers/carcas/header/index.jsx
+++ src/react/components/containers/carcas/header/index.jsx
@@ -78,8 +78,15 @@ const Header = ({
 
     <header className = {`fixed flex items-center justify-between ${ s.header } relative`}>
 
-      <div className = {`${ s['auth-back'] } ${ isAuthStarted && s['auth-back--opened'] } ${ s.white_blur } absolute`}/>
+      <div className = {`${ s['auth-back'] } ${ isAuthStarted && s['auth-back--opened'] }  ${ s.white_blur } absolute`}/>
 
+      <div className = {`flex items-center justify-center ${ s['auth-back'] } ${ isAuthStarted && s['auth-back--opened'] } ${ s.circles_container } ${ isAuthStarted ? s['circles_container--appear'] : s['circles_container--disappear'] } absolute`}>
+        
+        <div className = {`${ s.circles_container__blue_circle } absolute`}/>
+        <div className = {`${ s.circles_container__green_circle } absolute`}/>
+
+      </div>
+      
       <div
       
         id = "logocircle"
