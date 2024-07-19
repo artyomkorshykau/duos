5ed7a93395f5db9f3b621ee79133c433edc491e7
atryomkorshykau <butovichjunior@gmail.com>
2024-07-19 13:47:04 +0300

    add icons form.logo / notise

---
+++ src/react/components/icons/form.logo.continue.jsx
@@ -0,0 +1,222 @@
+import * as React from "react"
+import { memo } from "react"
+const SvgComponent = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={160}
+    height={160}
+    fill="none"
+    {...props}
+  >
+    <rect width={91} height={127} x={34.5} y={18.5} stroke="#CDDEFF" rx={9.5} />
+    <path
+      fill="#D1E3F7"
+      fillRule="evenodd"
+      d="M50 42a7 7 0 1 0 0-14 7 7 0 0 0 0 14Zm3.664-8.47a.85.85 0 0 0-1.327-1.061l-3.407 4.258L47.6 35.4A.85.85 0 0 0 46.4 36.6l2.672 2.672 4.593-5.742ZM50 73a7 7 0 1 0 0-14 7 7 0 0 0 0 14Zm3.664-8.47a.85.85 0 0 0-1.327-1.061l-3.407 4.258L47.6 66.4A.85.85 0 0 0 46.4 67.6l2.672 2.671 4.593-5.741ZM50 104a7 7 0 1 0 0-14 7 7 0 0 0 0 14Zm3.664-8.47a.85.85 0 0 0-1.327-1.061l-3.407 4.258L47.6 97.4A.85.85 0 0 0 46.4 98.6l2.672 2.671 4.593-5.741Z"
+      clipRule="evenodd"
+    />
+    <rect width={57} height={10} x={61} y={30} fill="#D1E3F7" rx={5} />
+    <rect width={57} height={10} x={61} y={61} fill="#D1E3F7" rx={5} />
+    <rect width={57} height={10} x={61} y={92} fill="#D1E3F7" rx={5} />
+    <rect width={3} height={3} x={44} y={132} fill="#D1E3F7" rx={1.5} />
+    <rect width={3} height={3} x={49} y={132} fill="#D1E3F7" rx={1.5} />
+    <rect width={3} height={3} x={54} y={132} fill="#D1E3F7" rx={1.5} />
+    <rect width={47} height={6} x={61} y={42} fill="#EEF6FF" rx={3} />
+    <rect width={16} height={14} x={61} y={73} fill="#EEF6FF" rx={7} />
+    <rect width={16} height={14} x={80} y={73} fill="#EEF6FF" rx={7} />
+    <rect width={16} height={14} x={99} y={73} fill="#EEF6FF" rx={7} />
+    <rect width={32} height={6} x={61} y={50} fill="#EEF6FF" rx={3} />
+    <rect width={18} height={5} x={61} y={106} fill="#EEF6FF" rx={2.5} />
+    <rect width={23} height={5} x={61} y={120} fill="#EEF6FF" rx={2.5} />
+    <rect width={29} height={5} x={61} y={113} fill="#EEF6FF" rx={2.5} />
+    <g clipPath="url(#a)">
+      <circle cx={115} cy={130} r={24} fill="#fff" />
+      <mask
+        id="b"
+        width={42}
+        height={42}
+        x={94}
+        y={109}
+        maskUnits="userSpaceOnUse"
+        style={{
+          maskType: "luminance",
+        }}
+      >
+        <path
+          fill="#fff"
+          d="M115.015 109.431c-12.06 0-20.556 8.611-20.556 20.556s8.498 20.554 20.556 20.554c12.058 0 20.556-8.611 20.556-20.556s-8.496-20.555-20.556-20.555"
+        />
+      </mask>
+      <g mask="url(#b)">
+        <mask
+          id="c"
+          width={166}
+          height={94}
+          x={23}
+          y={70}
+          maskUnits="userSpaceOnUse"
+          style={{
+            maskType: "luminance",
+          }}
+        >
+          <path fill="#fff" d="M188.697 163.327H23.095V70.89h165.602v92.437Z" />
+        </mask>
+        <g mask="url(#c)">
+          <mask
+            id="d"
+            width={238}
+            height={226}
+            x={-13}
+            y={4}
+            maskUnits="userSpaceOnUse"
+            style={{
+              maskType: "luminance",
+            }}
+          >
+            <path
+              fill="#fff"
+              d="M54.943 229.253-12.974 88.667 156.851 4.969l67.917 140.586-169.825 83.698Z"
+            />
+          </mask>
+          <g mask="url(#d)">
+            <path
+              fill="url(#e)"
+              d="M32.747 323.895-100.491 48.096 179.052-89.676 312.29 186.123 32.747 323.895Z"
+            />
+          </g>
+        </g>
+        <mask
+          id="f"
+          width={166}
+          height={94}
+          x={23}
+          y={70}
+          maskUnits="userSpaceOnUse"
+          style={{
+            maskType: "luminance",
+          }}
+        >
+          <path fill="#fff" d="M188.697 163.327H23.095V70.89h165.602v92.437Z" />
+        </mask>
+        <g mask="url(#f)">
+          <mask
+            id="g"
+            width={238}
+            height={226}
+            x={-13}
+            y={4}
+            maskUnits="userSpaceOnUse"
+            style={{
+              maskType: "luminance",
+            }}
+          >
+            <path
+              fill="#fff"
+              d="M54.943 229.253-12.974 88.667 156.851 4.969l67.917 140.586-169.825 83.698Z"
+            />
+          </mask>
+          <g mask="url(#g)">
+            <path
+              fill="url(#h)"
+              d="M138.476 160.651H87.794l-.406-51.565h50.681l.407 51.565Z"
+            />
+          </g>
+        </g>
+        <mask
+          id="i"
+          width={42}
+          height={42}
+          x={74}
+          y={96}
+          maskUnits="userSpaceOnUse"
+          style={{
+            maskType: "luminance",
+          }}
+        >
+          <circle
+            cx={20.473}
+            cy={20.473}
+            r={20.473}
+            fill="#fff"
+            transform="matrix(1 0 0 -1 74.506 137.849)"
+          />
+        </mask>
+        <g mask="url(#i)">
+          <mask
+            id="j"
+            width={197}
+            height={196}
+            x={-15}
+            y={23}
+            maskUnits="userSpaceOnUse"
+            style={{
+              maskType: "luminance",
+            }}
+          >
+            <path
+              fill="#fff"
+              d="m57.512 218.346-72.42-115.796 124.251-79.275 72.419 115.795-124.25 79.276Z"
+            />
+          </mask>
+          <g mask="url(#j)">
+            <path
+              fill="url(#k)"
+              d="m81.093 167.287-13.327-71.59 71.452-13.97 13.328 71.59-71.453 13.97Z"
+            />
+          </g>
+        </g>
+      </g>
+      <path
+        fill="#fff"
+        d="M108.958 126.483c.243-2.125-.67-3.967-2.054-4.13-1.384-.163-2.745 1.695-2.883 2.9-.333 2.824.434 6.239 1.818 6.403 1.384.163 2.786-2.349 3.119-5.173Zm10.938.071c.3-2.118-.578-3.985-1.975-4.188-1.397-.202-2.821 1.617-2.991 2.818-.408 2.814.283 6.251 1.68 6.453 1.396.203 2.878-2.269 3.286-5.083Zm2.545-7.111c-1.461-1.071-3.161-1.649-4.824-1.894-1.138-.168-1.072 1.234.032 1.559 1.293.379 2.618.865 3.886 1.43.949.422 1.743-.481.906-1.095Zm-19.428-.672c1.624-.802 3.398-1.078 5.079-1.033 1.15.031.842 1.401-.301 1.53-1.339.151-2.728.4-4.074.738-1.008.252-1.635-.774-.704-1.235Zm13.231 18.03c-2.079-1.135-4.827-.752-7.263.349-1.892.854-.586 2.388 1.438 1.927 1.611-.366 3.247-.548 4.687-.489 1.023.041 2.037-1.297 1.138-1.787Z"
+      />
+    </g>
+    <defs>
+      <radialGradient
+        id="e"
+        cx={0}
+        cy={0}
+        r={1}
+        gradientTransform="matrix(86.07105 -42.4194 42.67974 86.59928 79.315 129.31)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#7252F8" />
+        <stop offset={0.07} stopColor="#7252F8" />
+        <stop offset={0.46} stopColor="#00E7FF" />
+        <stop offset={0.52} stopColor="#00E8EE" />
+        <stop offset={0.62} stopColor="#00ECC4" />
+        <stop offset={0.76} stopColor="#00F280" />
+        <stop offset={0.94} stopColor="#00FB24" />
+        <stop offset={1} stopColor="#0F0" />
+      </radialGradient>
+      <radialGradient
+        id="h"
+        cx={0}
+        cy={0}
+        r={1}
+        gradientTransform="matrix(-4.93738 -41.00733 92.35787 -11.1201 117.666 150.093)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#1E00C8" />
+        <stop offset={1} stopColor="#7158FF" />
+      </radialGradient>
+      <radialGradient
+        id="k"
+        cx={0}
+        cy={0}
+        r={1}
+        gradientTransform="rotate(40.136 -91.501 121.411) scale(114.357 114.844)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0.445} stopColor="#006F10" />
+        <stop offset={0.93} stopColor="#00FB24" />
+        <stop offset={1} stopColor="#2CFFF2" />
+      </radialGradient>
+      <clipPath id="a">
+        <path fill="#fff" d="M91 106h48v48H91z" />
+      </clipPath>
+    </defs>
+  </svg>
+)
+const FormLogoContinue = memo(SvgComponent)
+export default FormLogoContinue
