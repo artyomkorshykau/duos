387d22c5077ed34b4d76a19661ddf76ca0b169e5
atryomkorshykau <butovichjunior@gmail.com>
2024-07-19 13:52:39 +0300

    add ContextNotise for questionnaire feature

---
+++ src/react/features/questionnaire/ui/context.notise/index.jsx
@@ -0,0 +1,27 @@
+import s from "@/react/features/questionnaire/ui/questionnaire.module.scss";
+import NotiseInfo from "@/react/components/icons/notise.info";
+
+const ContextNotise = ({
+
+  status
+
+}) => {
+
+  if ( status === 'begin' ) {
+
+    return (
+
+      <div className = {`${ s.questionnaire__context_notise }`}>
+
+        <NotiseInfo fill = { '#18009E' }/>
+        <p className = {`${ s.questionnaire__context_notise__title }`}>{ `Все поля являются обязательными.` }</p>
+
+      </div>
+
+    )
+
+  }
+
+}
+
+export default ContextNotise
