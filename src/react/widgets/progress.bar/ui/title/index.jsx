8218c9e35d2943a013bbf9c26a4dc1b98a6984eb
atryomkorshykau <butovichjunior@gmail.com>
2024-07-23 14:20:30 +0300

    add progressbar widget

---
+++ src/react/widgets/progress.bar/ui/title/index.jsx
@@ -0,0 +1,17 @@
+import s from '../progress.bar.module.scss'
+
+const Title = () => {
+
+  return (
+
+    <p className = {`text-26 ${ s.progressBar__title }`}>
+
+      Профиль
+
+    </p>
+
+  )
+
+}
+
+export default Title
