c8a3d9c8c452d0c70f1f1bebe3b3bb8dab733aa8
atryomkorshykau <butovichjunior@gmail.com>
2024-07-23 15:46:07 +0300

    add progress status

--- src/react/widgets/quiz/ui/form.img/index.jsx
+++ src/react/widgets/quiz/ui/form.img/index.jsx
@@ -2,6 +2,7 @@ import FormLogoBegin from "@/react/components/icons/form.logo.begin";
 import FormLogoContinue from "@/react/components/icons/form.logo.continue";
 import FormLogoEnd from "@/react/components/icons/form.logo.end";
 import s from "@/react/widgets/quiz/ui/quiz.module.scss";
+import QuizProgress from '@/constants/quiz.progress';
 
 const FormLogo = ({
 
@@ -13,9 +14,9 @@ const FormLogo = ({
 
     <div className={`${ s.quiz__form_logo }`}>
 
-      { status === 'begin' && <FormLogoBegin/> }
-      { status === 'continue' && <FormLogoContinue/> }
-      { status === 'end' && <FormLogoEnd/> }
+      { status === QuizProgress.begin && <FormLogoBegin/> }
+      { status === QuizProgress.continue && <FormLogoContinue/> }
+      { status === QuizProgress.end && <FormLogoEnd/> }
 
     </div>
 
