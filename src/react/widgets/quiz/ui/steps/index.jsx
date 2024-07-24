83cfc300d1466f27b81da068b1750cbd750ef279
atryomkorshykau <butovichjunior@gmail.com>
2024-07-24 18:10:07 +0300

    fix tabs

--- src/react/widgets/quiz/ui/steps/index.jsx
+++ src/react/widgets/quiz/ui/steps/index.jsx
@@ -1,7 +1,6 @@
 import s from '@/react/widgets/quiz/ui/quiz.module.scss';
 import NotiseSuccessDisabled
   from '@/react/components/icons/notise.success.disabled';
-import useGlobal from '@/store';
 import QuizProgress from '@/constants/quiz.progress';
 import QuizSteps from '@/constants/quiz.steps';
 
@@ -16,24 +15,24 @@ status
 
   return (
 
-    <div className={ `${ s.quiz__steps }` }>
+    <div className = { `${ s.quiz__steps }` }>
 
       { QuizSteps.map( el => {
 
         return (
 
-          <div className={ `${ s.quiz__steps__item }` } key={ el.id }>
+          <div className = { `${ s.quiz__steps__item }` } key = { el.id }>
 
-            <p className={ `text-16 ${ s.quiz__steps__item__title } ${ disabled }` }>{ el.title }</p>
+            <p className = { `text-16 ${ s.quiz__steps__item__title } ${ disabled }` }>{ el.title }</p>
 
             { status !== QuizProgress.end
 
-              ? <p className={ `text-13 ${ s.quiz__steps__item__progress }` }>{ el.timeToComplete }</p>
+              ? <p className = { `text-13 ${ s.quiz__steps__item__progress }` }>{ el.timeToComplete }</p>
 
-              : <div className={ `flex gap-1` }>
+              : <div className = { `flex gap-1` }>
 
-                <p className={ `text-13 ${ s.quiz__steps__item__progress } ${ disabled }` }>{ `Завершено` }</p>
-                <NotiseSuccessDisabled fill={ '#D1E3F7' } />
+                <p className = { `text-13 ${ s.quiz__steps__item__progress } ${ disabled }` }>{ `Завершено` }</p>
+                <NotiseSuccessDisabled fill = { '#D1E3F7' } />
 
               </div>
 
