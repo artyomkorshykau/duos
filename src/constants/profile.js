63fb07327611bfe0de81010538dd7f88297b536a
atryomkorshykau <butovichjunior@gmail.com>
2024-07-29 15:30:50 +0300

    add constants for profile selects

---
+++ src/constants/profile.js
@@ -0,0 +1,31 @@
+export const genderList = [
+
+  { id: 1, value: 'male', label: 'Мужчина' },
+  { id: 2, value: 'female', label: 'Женщина' }
+
+]
+
+export const taxStatuesList = [
+
+  { id: 3, value: 'ooo', label: '"ООО"' },
+  { id: 4, value: 'ip', label: 'ИП' },
+  { id: 5, value: 'self-employed', label: 'Самозанятый' },
+  { id: 6, value: 'Individual', label: 'Физическое лицо' }
+
+]
+
+export const countryList = [
+
+  { id: 7, value: 'countryList', label: 'Список стран' },
+
+]
+
+export const cityList = [
+
+  { id: 8, value: 'cityList', label: 'Список городов по выбранной стране' },
+
+]
+
+
+
+
