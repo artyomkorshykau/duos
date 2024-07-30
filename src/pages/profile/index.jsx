b743139de4006354f3b98542e5e7585b71b766a4
atryomkorshykau <butovichjunior@gmail.com>
2024-07-30 13:02:49 +0300

    add school step and autoscroll to top

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -7,13 +7,34 @@ import Carcas from '@/react/components/containers/carcas';
 
 export default function ProfilePage() {
 
-  const [step, setStep] = useState('Profile')
+  const [ step, setStep ] = useState( 'Profile' )
+  const [ title, setTitle ] = useState( 'Профиль' )
+  const [ description, setDescription ] = useState( 'Эти данные станут частью вашего профиля и помогут продвижению' )
 
   const nextStep = () => {
 
-    step === 'Profile' && setStep('Services')
+    if( step === 'Profile' ) {
+
+      setStep( 'Services' )
+      setTitle('Услуги')
+      setDescription('В каких направлениях и какие услуги вы готовы оказывать вашим будущим клиентам')
+
+
+    }
+
+    if( step === 'Services' ) {
+
+      setStep( 'School' )
+      setTitle('Школа')
+      setDescription('Если у вас нет собственной школы или курса переходите к следующему шагу')
+
+    }
+
+    window.scrollTo(0, 0)
 
   }
+
+  console.log(nextStep.title)
 
   return (
 
@@ -27,10 +48,10 @@ export default function ProfilePage() {
 
         <div className = { `flex flex-column` }>
 
-          <ProgressBar />
-          <Profile step = { step }/>
+          <ProgressBar title = { title } description = { description }/>
+          <Profile step = { step } />
           <Autosave />
-          <Pagination nextStep = { nextStep }/>
+          <Pagination nextStep = { nextStep } />
 
         </div>
 
