43c0c37af6d4cd305e7befcf3e486b9ac162b137
atryomkorshykau <butovichjunior@gmail.com>
2024-07-30 13:04:38 +0300

    add School widget

---
+++ src/react/widgets/school/ui/index.jsx
@@ -0,0 +1,17 @@
+import SchoolName from '@/react/widgets/school/ui/school.name'
+import CourseName from '@/react/widgets/school/ui/course.name'
+import Comment from '@/react/widgets/school/ui/comment'
+
+const School = () => {
+
+  return <>
+
+    <SchoolName/>
+    <CourseName/>
+    <Comment/>
+
+  </>
+
+}
+
+export default School
