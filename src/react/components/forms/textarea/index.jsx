eddd2aa6b9778de237f38d0355e244eb5a956343
Stanislav <kepamuk@yandex.ru>
2024-07-31 15:50:46 +0300

    add custom resizer to textarea

--- src/react/components/forms/textarea/index.jsx
+++ src/react/components/forms/textarea/index.jsx
@@ -1,7 +1,16 @@
+'use client'
 
 import cssIf from '@/scripts/helpers/css.if';
 import s from './textarea.module.scss';
+import { useEffect, useState, useRef } from 'react';
 
+const TEXTAREA_SIZE = {
+
+  width: 944,
+  height: 110
+
+}
+  
 const Textarea = ( props ) => {
 
   const { 
@@ -16,7 +25,7 @@ const Textarea = ( props ) => {
     placeholder = "",
     className = "",
     inputClassName = "",
-    refDOM = null,
+    refDOM = null,  
     onClick = () => {},
     onKeyUp = () => {},
     onBlur = () => {},
@@ -25,11 +34,117 @@ const Textarea = ( props ) => {
 
   } = props;
 
+  const [width, setWidth] = useState(TEXTAREA_SIZE.width); 
+  const [height, setHeight] = useState(TEXTAREA_SIZE.height); 
+  const [isResizing, setIsResizing] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [startY, setStartY] = useState(0);
+  const textfieldDivRef = useRef(null);
+
+  const handleMouseDown = (e) => {
+
+    setIsResizing(true);
+    setStartX(e.clientX);
+    setStartY(e.clientY);
+
+  };
+
+  const handleMouseMove = (e) => {
+
+    if (isResizing) {
+
+      const newWidth = width + (e.clientX - startX);
+      const newHeight = height + (e.clientY - startY);
+
+      setWidth(newWidth);
+      setHeight(newHeight);
+      setStartX(e.clientX);
+      setStartY(e.clientY);
+
+    }
+
+  };
+
+  const handleMouseUp = () => {
+
+    setIsResizing(false);
+
+  };
+
+  const handleTouchStart = (e) => {
+    
+    setIsResizing(true);
+    setStartX(e.touches[0].clientX);
+    setStartY(e.touches[0].clientY);
+
+  };
+
+  const handleTouchMove = (e) => {
+
+    if (isResizing) {
+
+      const newWidth = width + (e.touches[0].clientX - startX);
+      const newHeight = height + (e.touches[0].clientY - startY);
+      setWidth(newWidth);
+      setHeight(newHeight);
+      setStartX(e.touches[0].clientX);
+      setStartY(e.touches[0].clientY);
+
+    }
+
+  };
+
+  const handleTouchEnd = () => {
+
+    setIsResizing(false);
+
+  };
+
+  useEffect(() => {
+
+    const elementWidth = textfieldDivRef.current.offsetWidth;
+    
+    if (width > elementWidth) {
+
+      setWidth(elementWidth);
+
+    }
+
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('mouseup', handleMouseUp);
+    window.addEventListener('touchmove', handleTouchMove);
+    window.addEventListener('touchend', handleTouchEnd);
+
+    return () => {
+
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseup', handleMouseUp);
+      window.removeEventListener('touchmove', handleTouchMove);
+      window.removeEventListener('touchend', handleTouchEnd);
+  
+    };
+
+  }, [isResizing, width, height]);
+
   return (
     
-    <div className = {`${ s.textfield } ${ cssIf( error === "", s['textfield--error'] ) } ${ className }`}>
+    <div  
+
+      className = { `${ s.textfield } ${ cssIf( error === "", s['textfield--error'] ) } ${ className }` }
+      ref={ textfieldDivRef }
+      style={ { width: `${width}px`, height: `${height}px` } }
+      
+      >
 
       { !!title && <p className = { s.title }>{ title }</p> }
+
+      <div 
+
+        className={ s.resizer } 
+        onMouseDown={ handleMouseDown }
+        onTouchStart={ handleTouchStart }
+
+        ></div>
       
       <textarea 
 
@@ -42,13 +157,13 @@ const Textarea = ( props ) => {
         onChange = { onChange }
         onKeyUp = { onKeyUp }
         onBlur = { onBlur }
-        {...inputParams}
+        { ...inputParams }
         onClick = {( e ) => {
           
           onClick && onClick();
           e.stopPropagation();
 
-        }}
+        }} 
       
       />
 
