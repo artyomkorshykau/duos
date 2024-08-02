f610baa4714530a0d5c0d9d82468af892273c47d
stas <kepamuk@yandex.ru>
2024-08-02 10:39:24 +0300

    update page index js

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,10 @@
 import Carcas from "@/react/components/containers/carcas";
-import PublicationCard from "@/react/components/publication.card";
 
 export default function Home() {
 
   return (
 
-    <main id = {``} className = {`flex items-center justify-center h-dvh`}>
+      <main id = {``} className = {`flex items-center justify-center h-dvh`}>
 
       <Carcas
 
