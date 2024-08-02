d1ae8443d72f39ca1c0811f7cae7ff34a0fe0392
atryomkorshykau <butovichjunior@gmail.com>
2024-08-02 19:57:26 +0300

    add publications step

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -7,10 +7,11 @@ import Carcas from '@/react/components/containers/carcas';
 import Services from '@/react/widgets/services/ui';
 import School from '@/react/widgets/school/ui'
 import Document from '@/react/widgets/document/ui';
+import Publications from "@/react/widgets/publications/ui";
 
 export default function ProfilePage() {
 
-  const [ step, setStep ] = useState( 'Профиль' )
+  const [ step, setStep ] = useState( 'Публикации' )
   const [ title, setTitle ] = useState( 'Профиль' )
   const [ description, setDescription ] = useState( 'Эти данные станут частью вашего профиля и помогут продвижению' )
 
@@ -37,6 +38,14 @@ export default function ProfilePage() {
 
       setStep( 'Документы' )
       setTitle('Документы')
+      setDescription('Сертификаты, отзывы и прочая информация относительно всего, что вы заполняли ранее')
+
+    }
+
+    if( step === 'Документы' ) {
+
+      setStep( 'Публикации' )
+      setTitle('Публикации')
       setDescription('Сертификаты, отзывы и прочая информация относительно всего, что вы заполняли ранее')
 
     }
@@ -80,6 +89,16 @@ export default function ProfilePage() {
       return (
 
         <Document />
+
+      )
+
+    }
+
+    if ( step === 'Публикации' ) {
+
+      return (
+
+        <Publications />
 
       )
 
