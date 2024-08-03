40ce03bc19faaf7ffdfea9333378d9cc1d430630
atryomkorshykau <butovichjunior@gmail.com>
2024-08-03 16:58:47 +0300

    update Publications widget

--- src/react/widgets/publications/ui/index.jsx
+++ src/react/widgets/publications/ui/index.jsx
@@ -1,97 +1,20 @@
-import useGlobal from "@/store";
+"use client"
+
 import AccordionParent from "@/react/widgets/accordion.parent";
 import s from './publications.module.scss'
-import PublicationCard from "@/react/components/publication.card";
-import Textarea from "@/react/components/forms/textarea";
-import Attachment from "@/react/components/attachment";
+import PublicationsContent from "@/react/widgets/publications/ui/publications";
+import AboutYourselfContent
+  from "@/react/widgets/publications/ui/about.yourself";
+import useGlobal from "@/store";
 
 const Publications = () => {
 
-  const [ globalState, globalActions ] = useGlobal()
-
-  const { publications } = globalState;
+  const [ globalState ] = useGlobal()
 
   const content = (index) => {
 
-    if( index === 0 ) {
-
-      return (
-
-        <>
-
-        { publications.categories[index].profileInfo.map( section => {
-
-            return (
-
-              <div key = { section.id }>
-
-                <h4 className = {`text-20 ${ s.publicationsWrapper__title }`}>{ section.title }</h4>
-                <p className = {`text-16 ${ s.publicationsWrapper__description }`}>{ section.description }</p>
-                { section.id
-
-                  ? <Textarea placeholder = { section.placeholder }/>
-                  : <Attachment/>
-
-                }
-
-              </div>
-
-            )
-
-          })
-
-        }
-
-        </>
-
-      )
-
-    }
-
-    if( index === 1 ) {
-
-     return (
-
-       <div className = {`flex justify-start flex-wrap gap-24`}>
-
-         <PublicationCard/>
-
-         <PublicationCard
-
-           title = 'Как преодолеть тоску, когда не знаешь, в чем ее причина?'
-           status = { 'NotFilled' }
-
-         />
-
-         <PublicationCard
-
-           title = 'Как преодолеть тоску, когда не знаешь, в чем ее причина?'
-           description = {`Мы не всегда понимаем то, что чувствуем и тем более не всегда ясно, 
-         что стало причиной этих ощущений. Давайте найдем ответ вместе и освободимся от этой тяжести на душе. 
-         Мы не всегда понимаем то, что чувствуем и тем более не всегда ясно, что стало причиной этих ощущений. 
-         Давайте найдем ответ вместе и освободимся от этой тяжести на душе`}
-           status = { 'Filled' }
-           photo = { 'img/roles/expert.jpg' }
-
-         />
-
-         <PublicationCard
-
-           title = 'Как преодолеть тоску, когда не знаешь, в чем ее причина?'
-           description = {`Мы не всегда понимаем то, что чувствуем и тем более не всегда ясно, 
-         что стало причиной этих ощущений. Давайте найдем ответ вместе и освободимся от этой тяжести на душе. 
-         Мы не всегда понимаем то, что чувствуем и тем более не всегда ясно, что стало причиной этих ощущений. 
-         Давайте найдем ответ вместе и освободимся от этой тяжести на душе`}
-           status = { 'Filled' }
-           photo = { 'img/roles/expert.jpg' }
-
-         />
-
-       </div>
-
-     )
-
-    }
+    if( index === 0 ) return <AboutYourselfContent index = { index }/>
+    if( index === 1 ) return <PublicationsContent/>
 
   }
 
@@ -99,7 +22,7 @@ const Publications = () => {
 
     <div className = {`${ s.publicationsWrapper }`}>
 
-      { publications.categories.map( (category, index) => {
+      { globalState.publications.categories.map( (category, index) => {
 
         return (
 
