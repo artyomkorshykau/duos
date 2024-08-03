32bace2082dde0d3ede8ad5d7d8c0bc996686865
atryomkorshykau <butovichjunior@gmail.com>
2024-08-03 16:59:37 +0300

    update publications state

--- src/store/states/publications.js
+++ src/store/states/publications.js
@@ -6,7 +6,7 @@ const publicationsState = {
 
       id: 0,
       title: "О себе",
-      status: "NotFinished",
+      status: "NotFilled",
       documentStatus: "NotFilled",
       description: 'Загрузите хорошее фото, расскажите о себе и создайте несколько публикаций, тогда наши пользователи смогут найти вас быстрее, а вы получите возможность быстро найти новых читателей и потенциальных клиентов с первых дней',
       profileInfo: [
@@ -50,7 +50,7 @@ const publicationsState = {
 
         }
 
-      ]
+      ],
 
     },
     {
@@ -60,6 +60,34 @@ const publicationsState = {
       status: "NotFinished",
       documentStatus: "NotFilled",
       description: 'Создайте до 5 публикаций (статьи, кейсы, тематические посты, интересные материалы о Вашей работе, отзывы). Эти публикации будут размещены в вашем профиле и значительно повлияют на ваше продвижение.',
+      publicationsCards: [
+
+        {
+
+          title: 'Как преодолеть тоску, когда не знаешь, в чем ее причина?',
+          status: 'NotFilled',
+
+        },
+        {
+
+          title: 'Как преодолеть тоску, когда не знаешь, в чем ее причина?',
+          description: 'Мы не всегда понимаем то, что чувствуем и тем более не всегда ясно, что стало причиной этих' +
+            ' ощущений. Давайте найдем ответ вместе и освободимся от этой тяжести на душе. Мы не всегда понимаем то, что чувствуем и тем более не всегда ясно, что стало причиной этих ощущений. Давайте найдем ответ вместе и освободимся от этой тяжести на душ',
+          status: 'Filled',
+          photo: 'img/roles/expert.jpg'
+
+        },
+        {
+
+          title: 'Как преодолеть тоску, когда не знаешь, в чем ее причина?',
+          description: 'Мы не всегда понимаем то, что чувствуем и тем более не всегда ясно, что стало причиной этих' +
+            ' ощущений. Давайте найдем ответ вместе и освободимся от этой тяжести на душе. Мы не всегда понимаем то, что чувствуем и тем более не всегда ясно, что стало причиной этих ощущений. Давайте найдем ответ вместе и освободимся от этой тяжести на душ',
+          status: 'Filled',
+          photo: 'img/roles/expert.jpg'
+
+        },
+
+      ]
 
     },
 
@@ -67,4 +95,27 @@ const publicationsState = {
 
 }
 
-export default publicationsState
+const getInitialPublicationsState = () => {
+
+  if (typeof window !== "undefined") {
+
+    const storedPublicationsState = JSON.parse(localStorage.getItem("publications"))
+
+    if (storedPublicationsState) {
+
+      return storedPublicationsState
+
+    } else {
+
+      localStorage.setItem("publications", JSON.stringify(publicationsState))
+
+      return publicationsState
+
+    }
+
+  }
+
+  return publicationsState
+}
+
+export default getInitialPublicationsState
