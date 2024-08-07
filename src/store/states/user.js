583f32a377cea6da074ff2459dad6becfab1f46e
atryomkorshykau <butovichjunior@gmail.com>
2024-08-07 10:50:09 +0300

    update fakedata

--- src/store/states/user.js
+++ src/store/states/user.js
@@ -1,10 +1,16 @@
-const FAKEDATA = {
+const FAKEDATA = [
 
-  userName: "Иван",
-  userLastName: "Иванович",
-  userAvatar: "/img/test.default.data/avatar.jpeg",
-  userRole: "expert",
+  {
 
-}
+    userName: "Иван",
+    userLastName: "Иванович",
+    userAvatar: "/img/test.default.data/avatar.jpeg",
+    userRole: "expert",
+    password: '1234567890',
+    phone: '79955358190'
+
+  }
+
+]
 
 export default FAKEDATA;
