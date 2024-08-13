7d893e994a1726a9daf9db0b3cbdfa2be3e112b2
atryomkorshykau <butovichjunior@gmail.com>
2024-08-13 13:55:12 +0300

    custom scrollbar

--- src/pages/questionnaire/index.jsx
+++ src/pages/questionnaire/index.jsx
@@ -1,188 +1,34 @@
-import { useMemo, useState } from 'react';
 import ProgressBar from '@/react/widgets/progress.bar/ui';
-import Profile from '@/react/widgets/profile/ui';
 import Pagination from '@/react/widgets/pagination/ui';
 import Autosave from '@/react/widgets/autosave/ui';
 import Carcas from '@/react/components/containers/carcas';
-import Services from '@/react/widgets/services/ui';
-import School from '@/react/widgets/school/ui'
-import Document from '@/react/widgets/document/ui';
-import Publications from "@/react/widgets/publications/ui";
 import Quiz from "@/react/widgets/quiz/ui";
-import { useQuiz } from "@/react/widgets/quiz/model";
 import { steps } from "@/constants/quiz.steps";
-import QuizProgress from "@/constants/quiz.progress";
+import { useQuestionnaire } from "@/pages/questionnaire/model";
+import { useMemo } from "react";
+import s from './questionnaire.module.scss'
 
 export default function ProfilePage() {
 
-  const [ step, setStep ] = useState( steps.questionnaire )
-  const [ title, setTitle ] = useState( 'Профиль' )
-  const [ description, setDescription ] = useState( 'Эти данные станут частью вашего профиля и помогут продвижению' )
   const {
 
-    buttonTitle,
-    handleButtonAction,
     status,
-    setStatus
+    prevStep,
+    nextStep,
+    buttonTitle,
+    quizContent,
+    handleButtonAction,
+    step,
+    title,
+    description
 
-  } = useQuiz( { setStep } )
+  } = useQuestionnaire()
 
-  const nextStep = () => {
+  const content = useMemo(() => (
 
-    if( step === steps.profile ) {
+    <div className = { `${ s.content }`}>
 
-      setStep( steps.service )
-      setTitle('Услуги')
-      setDescription('В каких направлениях и какие услуги вы готовы оказывать вашим будущим клиентам')
-
-
-    }
-
-    if( step === steps.service ) {
-
-      setStep( steps.school )
-      setTitle('Школа')
-      setDescription('Если у вас нет собственной школы или курса переходите к следующему шагу')
-
-    }
-
-    if( step === steps.school ) {
-
-      setStep( steps.documents )
-      setTitle('Документы')
-      setDescription('Сертификаты, отзывы и прочая информация относительно всего, что вы заполняли ранее')
-
-    }
-
-    if( step === steps.documents ) {
-
-      setStep( steps.publications )
-      setTitle('Публикации')
-      setDescription('Сертификаты, отзывы и прочая информация относительно всего, что вы заполняли ранее')
-
-    }
-    if( step === steps.publications ) {
-
-      setStatus(QuizProgress.end)
-      setStep( steps.questionnaire )
-
-    }
-
-    window.scrollTo({
-
-      top: 0,
-      behavior: 'smooth'
-
-    })
-
-  }
-
-  const prevStep = () => {
-
-    if( step === steps.service ) {
-
-      setStep( steps.profile )
-      setTitle('Профиль')
-      setDescription('Эти данные станут частью вашего профиля и помогут продвижению')
-
-    }
-
-    if( step === steps.school ) {
-
-      setStep( steps.service )
-      setTitle('Услуги')
-      setDescription('В каких направлениях и какие услуги вы готовы оказывать вашим будущим клиентам')
-
-    }
-
-    if( step === steps.documents ) {
-
-      setStep( steps.school )
-      setTitle('Школа')
-      setDescription('Если у вас нет собственной школы или курса переходите к следующему шагу')
-
-    }
-    if( step === steps.publications ) {
-
-      setStep( steps.documents )
-      setTitle('Документы')
-      setDescription('Сертификаты, отзывы и прочая информация относительно всего, что вы заполняли ранее')
-
-    }
-
-    window.scrollTo({
-
-      top: 0,
-      behavior: 'smooth'
-
-    })
-
-  }
-
-  const content = useMemo(() => {
-
-    if ( step === steps.profile ) {
-
-      return (
-
-        <Profile />
-
-      )
-
-    }
-    if ( step === steps.service ) {
-
-      return (
-
-        <Services />
-
-      )
-
-    }
-
-    if ( step === steps.school ) {
-
-      return (
-
-        <School/>
-
-      )
-
-    }
-
-    if ( step === steps.documents ) {
-
-      return (
-
-        <Document />
-
-      )
-
-    }
-
-    if ( step === steps.publications ) {
-
-      return (
-
-        <Publications />
-
-      )
-
-    }
-
-  }, [ step ])
-
-  return (
-
-    <main id = {``} className = {`${ step === steps.questionnaire && 'flex items-center h-dvh' }`}>
-
-      <Carcas
-
-        authorized = { true }
-
-      >
-
-        { step === steps.questionnaire
+      { step === steps.questionnaire
 
         ? <Quiz
 
@@ -190,7 +36,7 @@ export default function ProfilePage() {
           handleButtonAction = { handleButtonAction }
           status = { status }
 
-          />
+        />
         : <div className = { `flex flex-column` }>
 
           <ProgressBar
@@ -201,7 +47,7 @@ export default function ProfilePage() {
 
           />
 
-          { content }
+          { quizContent }
 
           <Autosave/>
           <Pagination
@@ -214,7 +60,23 @@ export default function ProfilePage() {
 
         </div>
 
-        }
+      }
+
+    </div>
+
+  ), [ step ] )
+
+  return (
+
+    <main id = {``} className = {`${ step === steps.questionnaire && 'flex items-center h-dvh' }`}>
+
+      <Carcas
+
+        authorized = { true }
+
+      >
+
+        { content }
 
       </Carcas>
 
