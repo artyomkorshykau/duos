f1c01a215c33ef84e9f4fade7e563719cfdad88a
atryomkorshykau <butovichjunior@gmail.com>
2024-08-15 15:27:19 +0300

    fix checkbox

--- src/react/components/icons/check.mark.icon.jsx
+++ src/react/components/icons/check.mark.icon.jsx
@@ -1,7 +1,7 @@
 const CheckMarkIcon = ({ className }) => (
 
   <svg className = { className } width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M4 8.5L7 11.5L12 5.5" stroke="#E1EBF9" strokeWidth="1.7" strokeLinecap="round"/>
+    <path d="M4 8.5L7 11.5L12 5.5" strokeWidth="1.7" strokeLinecap="round"/>
   </svg>
 
 )
