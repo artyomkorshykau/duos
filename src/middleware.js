ca15ed91260cad72167cec2f37dec4fc2c3deaeb
atryomkorshykau <butovichjunior@gmail.com>
2024-09-11 20:02:49 +0300

    fixes middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,29 +1,20 @@
 import { NextResponse } from 'next/server'
 
 export async function middleware( request ) {
-
   const { cookies } = request
-  const token = cookies.get('token')
-  const url = request.nextUrl.clone()
-
-  if ( url.pathname === '/questionnaire'
-    || url.pathname === '/profile' ) {
-
+  const token = cookies.get( 'token' )
+  
+  if ( [ '/questionnaire', '/profile' ].includes( request.nextUrl.pathname ) ) {
     if ( !token ) {
-
-      url.pathname = '/'
-      return NextResponse.redirect(url)
-
+      return NextResponse.redirect( new URL( '/', request.url ) )
     }
-
   }
-
+  
   return NextResponse.next()
-
 }
 
 export const config = {
-
+  
   matcher: [ '/questionnaire', '/profile' ]
-
+  
 }
