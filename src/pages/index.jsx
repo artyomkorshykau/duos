efed0075ef44d65d83843d7d5cbd858841b55902
atryomkorshykau <butovichjunior@gmail.com>
2024-09-18 16:58:27 +0300

    dom loaded trigger

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,25 +1,29 @@
 import Carcas from '@/react/components/containers/carcas'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import useGlobal from '@/store/index.js'
 
 export default function Home() {
   
   const [ globalState, globalActions ] = useGlobal()
   const { profile } = globalActions
-  
-  
+  const [ domLoaded, setDomLoaded ] = useState( false )
   
   useEffect( () => {
     
     profile.getLocations()
-    
-    
+    setDomLoaded( true )
     
   }, [] )
   
+  if ( !domLoaded ) {
+    
+    return null
+    
+  }
+  
   return (
     
-    <main className = { `flex justify-center items-center h-dvh` }>
+    <main className={ `flex justify-center items-center h-dvh` }>
       
       <Carcas
       
