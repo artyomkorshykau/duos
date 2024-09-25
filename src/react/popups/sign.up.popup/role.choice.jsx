3313f92d450a525fcb8cea1bfd8935f5d3cd3421
atryomkorshykau <butovichjunior@gmail.com>
2024-09-25 14:20:54 +0300

    disabled client button

--- src/react/popups/sign.up.popup/role.choice.jsx
+++ src/react/popups/sign.up.popup/role.choice.jsx
@@ -1,79 +1,88 @@
-import Link from 'next/link'
 import s from './sign.up.module.scss'
+import DefaultButton from '@/react/components/buttons/default.button/index.jsx'
+import { useRouter } from 'next/navigation'
 
-const RoleChoice = ({
+const RoleChoice = ( {
+                       
+                       close = () => {}
+                       
+                     } ) => {
   
-  close = () => {}
-
-}) => {
-
+  const { push } = useRouter()
+  
   return (
-
-    <div className = { s.rolechoice }>
-
-      <p className = {`font-bold text-40 ${ s.rolechoice__title }`}>В какой роли вы хотите <br />зарегистрироваться?</p>
-
-      <div className = "flex justify-center gap-24">
-
-        <div className = {`flex flex-col item-center ${ s.rolechoice__card } relative`}>
-
-          <img src = "/img/roles/client.jpg"  className = { s.rolechoice__card__image }/>
-
-          <div className = {`absolute ${ s.rolechoice__card__textspace }`}>
-
-            <p className = {`font-bold text-26 ${ s.rolechoice__card__textspace__title }`}>Клиент</p>
-
-            <p className = {`font-semibold text-13 ${ s.rolechoice__card__textspace__text }`}>Откройте еще больше <br />уникального контента для <br />вашего саморазвития</p>
-
-            <div
+    
+    <div className={ s.rolechoice }>
+      
+      <p className={ `font-bold text-40 ${ s.rolechoice__title }` }>В какой роли
+        вы хотите <br/>зарегистрироваться?</p>
+      
+      <div className="flex justify-center gap-24">
+        
+        <div
+          className={ `flex flex-col item-center ${ s.rolechoice__card } relative` }>
+          
+          <img src="/img/roles/client.jpg"
+               className={ s.rolechoice__card__image }/>
+          
+          <div className={ `absolute ${ s.rolechoice__card__textspace }` }>
             
-              onClick = { () => close() }
-              className = {`flex items-center justify-center font-semibold text-14 ${ s.rolechoice__card__textspace__button } pointer`}
+            <p
+              className={ `font-bold text-26 ${ s.rolechoice__card__textspace__title }` }>Клиент</p>
+            
+            <p
+              className={ `font-semibold text-13 ${ s.rolechoice__card__textspace__text }` }>Откройте
+              еще больше <br/>уникального контента для <br/>вашего саморазвития
+            </p>
+            
+            <DefaultButton
               
-            >
-
-              Зарегистрироваться
-              
-            </div>
-
+              className={ `flex items-center justify-center font-semibold text-14 ${ s.rolechoice__card__textspace__button }` }
+              gray
+              name={ 'Зарегистрироваться' }
+              disabled
+            
+            />
+          
           </div>
-
+        
         </div>
-
-        <div className = {`flex flex-col item-center ${ s.rolechoice__card } relative`}>
-
-          <img src = "/img/roles/expert.jpg"  className = { s.rolechoice__card__image }/>
-
-          <div className = {`absolute ${ s.rolechoice__card__textspace }`}>
-
-            <p className = {`font-bold text-26 ${ s.rolechoice__card__textspace__title }`}>Эксперт</p>
-
-            <p className = {`font-semibold text-13 ${ s.rolechoice__card__textspace__text }`}>Заполните анкету и получите <br />возможность делиться своими <br />знаниями и опытом с людьми</p>
-
-            <Link href = "/questionnaire">
-
-              <div
+        
+        <div
+          className={ `flex flex-col item-center ${ s.rolechoice__card } relative` }>
+          
+          <img src="/img/roles/expert.jpg"
+               className={ s.rolechoice__card__image }/>
+          
+          <div className={ `absolute ${ s.rolechoice__card__textspace }` }>
+            
+            <p
+              className={ `font-bold text-26 ${ s.rolechoice__card__textspace__title }` }>Эксперт</p>
+            
+            <p
+              className={ `font-semibold text-13 ${ s.rolechoice__card__textspace__text }` }>Заполните
+              анкету и получите <br/>возможность делиться своими <br/>знаниями и
+              опытом с людьми</p>
+            
+            <DefaultButton
               
-                className = {`flex items-center justify-center font-semibold text-14 ${ s.rolechoice__card__textspace__button } pointer`}
-                
-              >
-
-                Заполнить анкету
-                
-              </div>
-
-            </Link>
-
+              className={ `flex items-center justify-center font-semibold text-14 ${ s.rolechoice__card__textspace__button }` }
+              gray
+              name={ 'Заполнить анкету' }
+              action={ () => push( '/questionnaire' ) }
+            
+            />
+          
           </div>
-
+        
         </div>
-
+      
       </div>
-
+    
     </div>
-
+  
   )
-
+  
 }
 
-export default RoleChoice;
+export default RoleChoice
